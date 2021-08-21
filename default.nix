6b3257c83d0f902f87a225d338a2fd34100cbe4d
Wojciech Siewierski <wojciech@siewierski.eu>
2021-08-21 17:14:41 +0200

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+stdenv.mkDerivation rec {
+  pname = "ssh-mv";
+  version = "1.0.0";
+  src = ./ssh_mv.py;
+
+  buildInputs = [
+    (python3.withPackages (pyPkgs: with pyPkgs; [
+      paramiko
+    ]))
+  ];
+
+  dontUnpack = true;
+  installPhase = "install -m755 -D $src $out/bin/ssh-mv";
+}
